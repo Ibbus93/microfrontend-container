ca4a60b782e6317790be65d0f84d3f0ce634b4cf
Ibbus93 <ibbus93@gmail.com>
2020-02-03 23:21:37 +0100

    Updates

---
+++ precache-manifest.77446e1f8a090000138f16edade26fff.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "76d26c74489c9225ca21",
+    "url": "/Ibbus93/microfrontend-container/static/js/main.f5a104a7.js"
+  },
+  {
+    "revision": "76d26c74489c9225ca21",
+    "url": "/Ibbus93/microfrontend-container/static/css/main.dfbf9474.css"
+  },
+  {
+    "revision": "749a955256eaa28fca43df11b66fd802",
+    "url": "/Ibbus93/microfrontend-container/index.html"
+  }
+];
