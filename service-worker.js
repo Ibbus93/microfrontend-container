a4b52438f299185dddf59450f20d86355f33f877
Federico Ibba <federicoibba@easy-network.it>
2020-02-04 16:56:11 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/microfrontend-container/precache-manifest.96f25db7bd6ca8239a7f9b303c051afe.js"
+  "/microfrontend-container/precache-manifest.65760b980798b6b2458d64362fd22f77.js"
 );
 
 workbox.clientsClaim();
