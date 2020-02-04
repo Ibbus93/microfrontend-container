683f3c3aa88880a8ce68124fa2f24c187ed28a59
Federico Ibba <federicoibba@easy-network.it>
2020-02-04 15:47:58 +0100

    Updates

---
+++ precache-manifest.1f57f14c4d60878128a61e8eceecc84e.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "c598b47a3c7f5581ce95",
+    "url": "/microfrontend-container/static/css/main.8dc8b610.css"
+  },
+  {
+    "revision": "c598b47a3c7f5581ce95",
+    "url": "/microfrontend-container/static/js/main.d4748bb7.js"
+  },
+  {
+    "revision": "67e3e16ba2bec29e05ee3c277ce1544d",
+    "url": "/microfrontend-container/index.html"
+  }
+];
